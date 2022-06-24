6be9f06eed2eecdf15f2875aee40ff710aba0719
New York Dev Ops <wallstreetx@icloud.com>
2022-06-24 17:54:54 -0400

    Update dim-keyword_ranker.py
    12

--- dim-keyword_ranker.py
+++ dim-keyword_ranker.py
@@ -156,13 +156,4 @@ if submit_button:
             listPosition.append(str(position))
 
             progressCount = progressCount + 1
-            progressBar = progressBar.progress(progressCount/(len(keywordQuery)))
-        
-        df[domainQuery] = listPosition
-        st.dataframe(df)
-
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()
-    st.markdown('### **⬇️ Download Output as CSV File **')
-    href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as "filename.csv")'
-    st.markdown(href, unsafe_allow_html=True)
+            progressBar = progressBar.progress(progressCount/(len(keywordQuery)));
